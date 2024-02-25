239326936ebb23bbc1de53c1f615dedde45953e9
Nikita <solodzhuk.ns@phystech.edu>
2024-02-25 19:50:51 +0300

    test ai behaviour

---
+++ Animals/Animals.cpp
@@ -0,0 +1,65 @@
+#include "Animals.hpp"
+
+void Animals::setCoords(int xCoord, int yCoord){
+    this->position = sf::Vector2f(xCoord, yCoord);
+}
+//true for colliding, false for not colliding
+bool Animals::circleCollision(Animals* animal){
+    sf::Vector2f diff = this->position - animal->position;
+    return (diff.x*diff.x + diff.y*diff.y) < 
+            ((this->collisionRadius + animal->collisionRadius) * 
+            (this->collisionRadius + animal->collisionRadius));
+};
+
+void Animals::move(sf::Vector2f direction, float delta){
+    sf::Vector2f newPosition = this->position + delta * direction * this->moveSpeed;
+    setCoords(newPosition.x, newPosition.y);
+}
+
+float Animals::getCollisionRadius(){
+    return this->collisionRadius;
+}
+
+float Animals::getSqrDistanceTo(Animals* animal){
+    sf::Vector2f distance = this->position - animal->position;
+    return distance.x*distance.x + distance.y*distance.y;
+}
+//possible attractors and repulsors inputs: Boar, Cow, Fox, Goose, Gorilla, Pig, Sheep, Wolf
+sf::Vector2f Animals::aiDirection(std::vector<Animals*>* animalArr, bool repulsorPriority = true){
+    sf::Vector2f entityDir = sf::Vector2f(0.0f, 0.0f);
+    Animals* closestRep = NULL;
+    Animals* closestAttr = NULL;
+
+    for (int i = 0; i < animalArr->size(); i++){
+        if ((*animalArr)[i] == this){
+            continue;
+        }
+        for (int j = 0; j < this->attractors.size(); j++){
+            if ((typeid((*animalArr)[i]).name() == (this->attractors)[j]) || 
+                (this->getSqrDistanceTo(closestAttr) > this->getSqrDistanceTo((*animalArr)[i]))){
+                if (closestAttr == NULL){
+                    closestAttr = (*animalArr)[i];
+                    break;
+                }
+            }
+        }
+        for (int j = 0; j < this->repulsors.size(); j++){
+            if ((typeid((*animalArr)[i]).name() == (this->repulsors)[j]) || 
+                (this->getSqrDistanceTo(closestRep) > this->getSqrDistanceTo((*animalArr)[i]))){
+                if (closestRep == NULL){
+                    closestRep = (*animalArr)[i];
+                    break;
+                }
+            }
+        }
+    }
+    if (repulsorPriority){
+        sf::Vector2f diff = closestRep->position - this->position;
+        float abs = sqrt(diff.x*diff.x + diff.y+diff.y);
+        return diff/abs;
+    }else{
+        sf::Vector2f diff = this->position - closestAttr->position;
+        float abs = sqrt(diff.x*diff.x + diff.y+diff.y);
+        return diff/abs;
+    }
+}
--- Animals/Animals.hpp
+++ Animals/Animals.hpp
@@ -1,19 +1,34 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <vector>
 #include "Food.hpp"
+#include "SFML/System/Vector2.hpp"
 
 using namespace std;
 
 class Animals
 {
 protected:
-    int legs;
+    sf::Vector2f position;
+    float collisionRadius, viewRarius;
+    bool lookDirection; //false for left, true for right
+    float moveSpeed;
 public:
+    // Animals *chase, *avoid;
+    sf::Vector2f aiDirection(std::vector<Animals*>*, bool);
+    float getSqrDistanceTo(Animals*);
+    bool circleCollision(Animals*);
+    void setCoords(int, int);
+    void move(sf::Vector2f, float);
     virtual void say() = 0;
     virtual bool canEat(Food *food) = 0;
+    float getCollisionRadius();
     ~Animals(){};
+    std::vector<string> attractors;
+    std::vector<string> repulsors;
 };
+
 class Herbivores : public Animals {
 public:
     bool canEat(Food *food){
