5c5d44e509164da7ca0d0861ff63f6feaed77e77
Zahar <zahar5070@gmail.com>
2024-03-16 19:11:47 +0300

    testing cmake

---
+++ build/CMakeFiles/zoo.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zahar/IKT/Class-inheritance/Animals/Animals.cpp" "CMakeFiles/zoo.dir/Animals/Animals.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Animals.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Boar.cpp" "CMakeFiles/zoo.dir/Animals/Boar.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Boar.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Cow.cpp" "CMakeFiles/zoo.dir/Animals/Cow.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Cow.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Fox.cpp" "CMakeFiles/zoo.dir/Animals/Fox.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Fox.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Goose.cpp" "CMakeFiles/zoo.dir/Animals/Goose.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Goose.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Gorilla.cpp" "CMakeFiles/zoo.dir/Animals/Gorilla.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Gorilla.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Pig.cpp" "CMakeFiles/zoo.dir/Animals/Pig.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Pig.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Sheep.cpp" "CMakeFiles/zoo.dir/Animals/Sheep.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Sheep.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Animals/Wolf.cpp" "CMakeFiles/zoo.dir/Animals/Wolf.cpp.o" "gcc" "CMakeFiles/zoo.dir/Animals/Wolf.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Drawer/Drawer.cpp" "CMakeFiles/zoo.dir/Drawer/Drawer.cpp.o" "gcc" "CMakeFiles/zoo.dir/Drawer/Drawer.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/Field/Field.cpp" "CMakeFiles/zoo.dir/Field/Field.cpp.o" "gcc" "CMakeFiles/zoo.dir/Field/Field.cpp.o.d"
+  "/home/zahar/IKT/Class-inheritance/main.cpp" "CMakeFiles/zoo.dir/main.cpp.o" "gcc" "CMakeFiles/zoo.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/zoo.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zoo.dir/Animals/Animals.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Animals.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Boar.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Boar.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Cow.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Cow.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Fox.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Fox.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Goose.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Goose.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Gorilla.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Gorilla.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Pig.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Pig.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Sheep.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Sheep.cpp.o.d"
+  "CMakeFiles/zoo.dir/Animals/Wolf.cpp.o"
+  "CMakeFiles/zoo.dir/Animals/Wolf.cpp.o.d"
+  "CMakeFiles/zoo.dir/Drawer/Drawer.cpp.o"
+  "CMakeFiles/zoo.dir/Drawer/Drawer.cpp.o.d"
+  "CMakeFiles/zoo.dir/Field/Field.cpp.o"
+  "CMakeFiles/zoo.dir/Field/Field.cpp.o.d"
+  "CMakeFiles/zoo.dir/main.cpp.o"
+  "CMakeFiles/zoo.dir/main.cpp.o.d"
+  "zoo"
+  "zoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
